3fd0d1b0f8e59e83e17688f10acd3933b9e6c0b6
Aman Kumar Rai <arai@reputation.com>
2021-12-05 22:49:30 +0530

    Search functionality

--- front-end/src/components/header/Header.js
+++ front-end/src/components/header/Header.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import './Header.css';
-import { IconButton } from '@mui/material';
+import { IconButton, TextField } from '@mui/material';
 
-const Header = () => {
+const Header = ({ searchText, setSearchText }) => {
+    const [showSearch, setShowSearch] = useState(false);
+
     return <div className="header-wrapper">
         <h1 className="header">Vocab</h1>
-        <IconButton size="large" aria-label="search" className="search-button">
-            <SearchIcon />
-        </IconButton>
+        <div className="search-wrapper">
+            <IconButton size="large" aria-label="search" className="search-button" onClick={() => {
+                setShowSearch(!showSearch);
+            }}>
+                <SearchIcon />
+            </IconButton>
+            {
+                showSearch && <TextField className="search-box" label="Search" variant="standard" value={searchText} onChange={(e) => {
+                    setSearchText(e.target.value)
+                }} />
+            }
+        </div>
     </div>
 };
 
--- front-end/src/components/home/Home.js
+++ front-end/src/components/home/Home.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../header/Header';
 import WordList from '../word-list/WordList';
+import axios from 'axios';
+import Loader from '../loader/Loader';
 
 import './Home.css';
 
 const Home = () => {
+    const [wordList, setWordList] = useState([]);
+    const [showLoader, setShowLoader] = useState(false);
+    const [searchText, setSearchText] = useState('');
+
+    const filterWords = (wordList, searchText) => {
+        return wordList.filter((word) => {
+            if (!searchText || searchText === '') {
+                return true
+            }
+            if (word.word.search(searchText) > -1) {
+                return true;
+            }
+
+            return false;
+        })
+    }
+
+    useEffect(() => {
+        refetchList();
+    }, [])
+
+    const refetchList = () => {
+        setShowLoader(true);
+        axios.get('http://localhost:8081/')
+            .then(function (response) {
+                setWordList(response.data);
+                setShowLoader(false);
+
+            })
+            .catch(function (error) {
+                console.log(error);
+                setShowLoader(false);
+
+            })
+    }
+
     return <div className="home">
-        <Header />
-        <WordList />
+        <Loader show={showLoader} />
+        <Header searchText={searchText} setSearchText={setSearchText} />
+        <WordList wordList={filterWords(wordList, searchText)} refetchList={refetchList} />
     </div>
 }
-
 
 export default Home;
